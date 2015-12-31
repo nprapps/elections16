13c640ee17f4fa474ada01a9a1ec070b05f7169e
Tyler Fisher <tyler@tylerjfisher.com>
2015-12-31 14:21:33 -0500

    another one

--- www/test/spec/app-spec.js
+++ www/test/spec/app-spec.js
@@ -1,13 +1,15 @@
-describe('app.js', function() {
+describe('candidates.json', function() {
     var candidates = [];
     beforeEach(function() {
         jasmine.getJSONFixtures().fixturesPath='base/data/';
-
         candidates = getJSONFixture('candidates.json')
-        console.log(candidates.length);
     });
 
     it('should have 60 candidates', function() {
         expect(candidates.length).toBe(60);
+    });
+
+    it('the first candidate should be Carly Fiorina', function() {
+        expect(candidates[0]['first'] + ' ' + candidates[0]['last']).toBe('Carly Fiorina');
     });
 });
