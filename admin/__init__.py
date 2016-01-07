f8c1164c9504feb1725a10a2ed78c7dd682e68e5
Tyler Fisher <tyler@tylerjfisher.com>
2016-01-07 16:28:21 -0500

    add flask admin, closes #77726596828151

--- admin/__init__.py
+++ admin/__init__.py
@@ -4,6 +4,9 @@ import json
 import logging
 
 from flask import Flask, make_response, render_template
+from flask_admin import Admin
+from flask_admin.contrib.peewee import ModelView
+from models import models
 from render_utils import make_context, smarty_filter, urlencode_filter
 from static.blueprint import static
 from werkzeug.debug import DebuggedApplication
@@ -25,6 +28,10 @@ app.register_blueprint(static, url_prefix='/%s' % app_config.PROJECT_SLUG)
 app.add_template_filter(smarty_filter, name='smarty')
 app.add_template_filter(urlencode_filter, name='urlencode')
 
+admin = Admin(app, url='/%s/admin' % app_config.PROJECT_SLUG)
+admin.add_view(ModelView(models.Result))
+admin.add_view(ModelView(models.Call))
+
 # Example application views
 @app.route('/%s/test/' % app_config.PROJECT_SLUG, methods=['GET'])
 def _test_app():
@@ -35,16 +42,13 @@ def _test_app():
 
     return make_response(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
 
-# Example of rendering index.html with admin_app 
+# Example of rendering index.html with admin_app
 @app.route ('/%s/' % app_config.PROJECT_SLUG, methods=['GET'])
 def index():
     """
     Example view rendering a simple page.
     """
     context = make_context(asset_depth=1)
-
-    with open('data/featured.json') as f:
-        context['featured'] = json.load(f)
 
     return make_response(render_template('index.html', **context))
 
