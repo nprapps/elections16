7016fd01d67b98bb0e3438aee4f8e1aee1990476
David Eads <davideads@gmail.com>
2016-01-07 00:28:49 -0500

    move all flask apps to their __init__.py files

---
+++ app/__init__.py
@@ -0,0 +1,36 @@
+import app_config
+import json
+
+from flask import Flask, make_response, render_template
+from oauth.blueprint import oauth, oauth_required
+from render_utils import make_context, smarty_filter, urlencode_filter
+from static.blueprint import static
+from werkzeug.debug import DebuggedApplication
+
+app = Flask(__name__)
+app.debug = app_config.DEBUG
+
+app.add_template_filter(smarty_filter, name='smarty')
+app.add_template_filter(urlencode_filter, name='urlencode')
+
+@app.route('/')
+@oauth_required
+def index():
+    """
+    Example view demonstrating rendering a simple HTML page.
+    """
+    context = make_context()
+
+    with open('data/featured.json') as f:
+        context['featured'] = json.load(f)
+
+    return make_response(render_template('index.html', **context))
+
+app.register_blueprint(static)
+app.register_blueprint(oauth)
+
+# Enable Werkzeug debug pages
+if app_config.DEBUG:
+    wsgi_app = DebuggedApplication(app, evalex=False)
+else:
+    wsgi_app = app
