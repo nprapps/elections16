23c97f31d242b345420de3c52135a256bb3b717a
Tyler Fisher <tyler@tylerjfisher.com>
2016-01-13 11:11:58 -0500

    initialize flickity in js

--- www/js/app.js
+++ www/js/app.js
@@ -1,13 +1,29 @@
 // Global jQuery references
+var $cards = null;
 
-//
+// Global references
 var candidates = {}
+var isTouch = Modernizr.touch;
 
 /*
  * Run on page load.
  */
 var onDocumentLoad = function(e) {
-    // Cache jQuery references
+    $cards = $('.cards');
+
+    setupFlickity();
+}
+
+var setupFlickity = function() {
+    $cards.height($(window).height());
+
+    $cards.flickity({
+        cellAlign: 'center',
+        draggable: isTouch,
+        wrapAround: true,
+        imagesLoaded: true,
+        pageDots: false
+    })
 }
 
 var getCandidates = function() {
