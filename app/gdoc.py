a082c2c29d0cacfada7f765f6341ff028926d550
David Eads <davideads@gmail.com>
2016-01-14 09:55:36 -0500

    clean up parsing

--- app/gdoc.py
+++ app/gdoc.py
@@ -1,3 +1,5 @@
+import urlparse
+
 from bs4 import BeautifulSoup
 
 WHITELIST_ATTRIBUTES = ['href']
@@ -15,26 +17,42 @@ class DocParser:
         self.parse()
 
     def parse(self):
-        for tag in self.soup.findAll():
-            self.remove_attrs(tag)
-            self.remove_empty(tag)
-
         for tag in self.soup.findAll('span'):
+            self.create_italic(tag)
+            self.create_strong(tag)
             self.unwrap_span(tag)
 
-    def remove_attrs(self, tag):
-        for k,v in tag.attrs.items():
-            if k not in WHITELIST_ATTRIBUTES:
-                del tag.attrs[k]
+        for tag in self.soup.findAll():
+            self.remove_empty(tag)
+            self.parse_attrs(tag)
+
+    def create_italic(self, tag):
+        if tag.get('style') == 'font-style:italic':
+            tag.wrap(self.soup.new_tag('em'))
+
+    def create_strong(self, tag):
+        if tag.get('style') == 'font-weight:bold':
+            tag.wrap(self.soup.new_tag('strong'))
 
     def unwrap_span(self, tag):
-        if tag.name == 'span':
-            tag.unwrap()
+        tag.unwrap()
+
+    def parse_attrs(self, tag):
+        for k, v in tag.attrs.items():
+            if k == 'href':
+                tag.attrs[k] = self.parse_href(v)
+            elif k != 'src':
+                del tag.attrs[k]
 
     def remove_empty(self, tag):
-        return
-        if not tag.text:
+        has_children = len(tag.contents)
+        has_text = len(list(tag.stripped_strings))
+        if not has_children and not has_text and not tag.is_empty_element:
             tag.extract()
+
+    def parse_href(self, href):
+        params = urlparse.parse_qs(urlparse.urlsplit(href).query)
+        return params.get('q')
 
     def __unicode__(self):
         return '\n'.join([unicode(tag) for tag in self.soup.body.children])
