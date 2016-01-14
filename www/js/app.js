0f498e10493bec8efd2c9e8ed37a57a85c8c5d98
Tyler Fisher <tyler@tylerjfisher.com>
2016-01-14 11:31:01 -0500

    dynamically show/hide global controls

--- www/js/app.js
+++ www/js/app.js
@@ -47,14 +47,20 @@ var onCardChange = function(e) {
     var newSlideIndex = flickity.selectedIndex;
 
     if (newSlideIndex > 0) {
-        $('.global-header').addClass('bg-header');
+        $('.global-controls').show();
     } else {
+        $('.global-controls').hide();
         $('.global-header').removeClass('bg-header');
     }
 }
 
 var onCardAnimationFinish = function(e) {
     var flickity = $cards.data('flickity');
+    var newSlideIndex = flickity.selectedIndex;
+
+    if (newSlideIndex > 0) {
+        $('.global-header').addClass('bg-header');
+    }
 }
 
 var getCandidates = function() {
