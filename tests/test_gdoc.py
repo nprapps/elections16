4a3b6230b3cf8d98183a377a203e8db613744c0c
Zyma Islam <zyma.islam90@gmail.com>
2016-01-14 15:18:02 -0500

    Finished running all tag tests on gdoc

--- tests/test_gdoc.py
+++ tests/test_gdoc.py
@@ -17,20 +17,20 @@ class DocParserTestCase(unittest.TestCase):
         self.contents = self.parser.soup.body.contents
 
     def test_h1(self):
-        self._test_tag(self.contents[0], 'h1')
+        self._is_tag(self.contents[0], 'h1')
 
     def test_h1_has_no_children(self):
         child_length = len(self.contents[0].find_all())
         self.assertEqual(child_length, 0)
 
     def test_h2(self):
-        self._test_tag(self.contents[1], 'h2')
+        self._is_tag(self.contents[1], 'h2')
 
     def test_h3(self):
-        self._test_tag(self.contents[2], 'h3')
+        self._is_tag(self.contents[2], 'h3')
 
     def test_p(self):
-        self._test_tag(self.contents[3], 'p')
+        self._is_tag(self.contents[3], 'p')
 
     def test_strong(self):
         self._contains_tag(self.contents[4], 'strong')
@@ -49,14 +49,14 @@ class DocParserTestCase(unittest.TestCase):
         self.assertEqual(href, 'http://npr.org')
 
     def test_ul(self):
-        self._test_tag(self.contents[8], 'ul')
+        self._is_tag(self.contents[8], 'ul')
 
     def test_ul_li(self):
         count_li = len(self.contents[8].find_all('li'))
         self.assertEqual(count_li, 3)
 
     def test_ol(self):
-        self._test_tag(self.contents[9], 'ol')
+        self._is_tag(self.contents[9], 'ol')
 
     def test_ol_li(self):
         count_li = len(self.contents[9].find_all('li'))
@@ -65,15 +65,25 @@ class DocParserTestCase(unittest.TestCase):
     def test_img(self):
         self._contains_tag(self.contents[11], 'img')
 
-    def _test_tag(self, tag, tag_name):
+    def test_strange_has_no_children(self):
+        child_length = len(self.contents[12].find_all())
+        self.assertEqual(child_length, 0)
+
+    def test_tabletag(self):
+        self._is_tag(self.contents[13], 'table')
+
+    def test_tabletd(self):
+        self._contains_tag(self.contents[13], 'td', 4)
+
+    def test_tabletr(self):
+        self._contains_tag(self.contents[13], 'tr', 2)
+
+    def _is_tag(self, tag, tag_name):
         self.assertEqual(tag.name, tag_name)
 
     def _contains_tag(self, tag, tag_name, count=1):
         child_length = len(tag.findAll(tag_name))
         self.assertEqual(child_length, count)
-
-
-
 
 if __name__ == '__main__':
     unittest.main()
