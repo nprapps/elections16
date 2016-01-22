0ae27720ed4d77904e8bdb264a17c602768775e4
David Eads <davideads@gmail.com>
2016-01-22 11:18:19 -0500

    working out more architecture details, still a lot of waiting

--- daemon/daemon.py
+++ daemon/daemon.py
@@ -3,11 +3,18 @@ from cement.core.foundation import CementApp
 from random import random
 
 LOOP_INTERVAL = 20
+SLOW_LOOP_INTERVAL = 60
+SLOW_LOOP_MODULO = SLOW_LOOP_INTERVAL / LOOP_INTERVAL
+
 
 class NPRElexDaemon(CementApp):
     class Meta:
         label = 'nprelexdaemon'
         extensions = ['daemon']
+
+    def slack_off(self):
+        for x in range(0, 20000000):
+            random()
 
 
 with NPRElexDaemon() as app:
@@ -18,14 +25,26 @@ with NPRElexDaemon() as app:
     while True:
         start = time()
 
-        count = count + 1
-        for x in range(0, 90000000):
-            random()
+        modulo = count % SLOW_LOOP_MODULO
+
+        if modulo == 0:
+            app.slack_off()
+            app.log.info('Per minute cycle hit (publish all content)')
+
+        app.log.info('20 second cycle hit (publish results)')
+        app.slack_off()
 
         end = time()
-
         wait = LOOP_INTERVAL - (end - start)
 
         print('Interval: %s' % count)
+        print('Modulo: %s' % modulo)
         print('Wait: %s' % wait)
-        sleep(wait)
+
+        count = count + 1
+
+        if wait > 0:
+            sleep(wait)
+
+
+
