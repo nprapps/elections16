d8242f805a2764441de59ec3455344887cc496a9
Tyler Fisher <tyler@tylerjfisher.com>
2016-01-25 16:14:19 -0500

    refactor sorting so we can reverse sort votecount

--- app/utils.py
+++ app/utils.py
@@ -126,8 +126,13 @@ def ap_time_period_filter(value):
 
     return periods
 
-def candidate_sorter(item):
+
+def candidate_sort_lastname(item):
     if item.last == 'Other' or item.last == 'Uncommitted':
-        return (item.votecount, 'zzz')
+        return 'zzz'
     else:
-        return (item.votecount, item.last)
+        return item.last
+
+
+def candidate_sort_lastname(item):
+    return item.votecount
