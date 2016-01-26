ae1b0854e6b3e0ed5d66749a30ee442461e46145
Tyler Fisher <tyler@tylerjfisher.com>
2016-01-26 16:57:27 -0500

    use an array of objects that define the listening times we want to log

--- www/js/audio.js
+++ www/js/audio.js
@@ -1,5 +1,33 @@
 var AUDIO = (function() {
     var NO_AUDIO = (window.location.search.indexOf('noaudio') >= 0);
+    var fiveSeconds = null;
+    var thirtySeconds = null;
+    var oneMinute = null;
+    var fiveMinutes = null;
+
+
+    var timedAnalytics = [
+        {
+            'seconds': 5,
+            'unit': '5s',
+            'measured': false
+        },
+        {
+            'seconds': 30,
+            'unit': '30s',
+            'measured': false
+        },
+        {
+            'seconds': 60,
+            'unit': '1m',
+            'measured': false
+        },
+        {
+            'seconds': 300,
+            'unit': '5m',
+            'measured': false
+        },
+    ]
 
     var setupAudio = function() {
         $audioPlayer.jPlayer({
@@ -17,6 +45,12 @@ var AUDIO = (function() {
             'mp3': url
         });
         playAudio();
+
+        var fiveSeconds = false;
+        var thirtySeconds = false;
+        var oneMinute = false;
+        var fiveMinutes = false;
+
         ANALYTICS.trackEvent('audio-started', url);
     }
 
@@ -60,6 +94,35 @@ var AUDIO = (function() {
         var totalTime = e.jPlayer.status.duration;
         var position = e.jPlayer.status.currentTime;
         var remainingTime = totalTime - position;
+
+        for (var i = 0; i < timedAnalytics.length; i++) {
+            var obj = timedAnalytics[i];
+
+            if (position >= obj.seconds && !obj.measured) {
+                ANALYTICS.trackEvent('audio-' + obj.unit, $audioPlayer.data().jPlayer.status.src);
+                obj.measured = true;
+            }
+        }
+
+        // if (position >= 5 && !fiveSeconds) {
+        //     ANALYTICS.trackEvent('audio-five-seconds', $audioPlayer.data().jPlayer.status.src)
+        //     fiveSeconds = true;
+        // }
+
+        // if (position >= 30 && !thirtySeconds) {
+        //     ANALYTICS.trackEvent('audio-thirty-seconds', $audioPlayer.data().jPlayer.status.src)
+        //     thirtySeconds = true;
+        // }
+
+        // if (position >= 60 && !oneMinute) {
+        //     ANALYTICS.trackEvent('audio-one-minute', $audioPlayer.data().jPlayer.status.src)
+        //     oneMinute = true;
+        // }
+
+        // if (position >= 300 && !fiveMinutes) {
+        //     ANALYTICS.trackEvent('audio-five-minutes', $audioPlayer.data().jPlayer.status.src)
+        //     fiveMinutes = true;
+        // }
 
         $duration.text($.jPlayer.convertTime(remainingTime));
     }
