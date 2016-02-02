2ef68be94ce2fe8c2f33368cbbe0b77a42303c7b
Alyson Hurt <ahurt@npr.org>
2016-02-02 09:54:18 -0500

    update gop delegate numbers

--- www/js/delegates.js
+++ www/js/delegates.js
@@ -2,10 +2,10 @@
 var DELEGATE_DATA = {
     "republicans": {
         "del_needed": 1237,
-        "last_updated": "Feb. 1, 2016, 11:56 p.m. EST",
+        "last_updated": "Feb. 2, 2016, 9:52 a.m. EST",
         "candidates": [
-            { "name_last": "Bush", "del_total": 0 },
-            { "name_last": "Carson", "del_total": 2 },
+            { "name_last": "Bush", "del_total": 1 },
+            { "name_last": "Carson", "del_total": 3 },
             { "name_last": "Christie", "del_total": 0 },
             { "name_last": "Cruz", "del_total": 8 },
             { "name_last": "Fiorina", "del_total": 0 },
@@ -13,14 +13,14 @@ var DELEGATE_DATA = {
             { "name_last": "Huckabee", "del_total": 0 },
             { "name_last": "Kasich", "del_total": 0 },
             { "name_last": "Paul", "del_total": 1 },
-            { "name_last": "Rubio", "del_total": 6 },
+            { "name_last": "Rubio", "del_total": 7 },
             { "name_last": "Santorum", "del_total": 0 },
             { "name_last": "Trump", "del_total": 7 }
         ]
     },
     "democrats": {
         "del_needed": 2382,
-        "last_updated": "Feb. 1, 2016, 11:56 p.m. EST",
+        "last_updated": "Feb. 2, 2016, 9:52 a.m. EST",
         "candidates": [
             { "name_last": "Clinton", "del_total": 384 },
             { "name_last": "Sanders", "del_total": 29 },
