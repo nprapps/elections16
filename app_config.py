7a17f06f15d0c6e4b4296c57149f963226dba3f0
David Eads <davideads@gmail.com>
2016-02-22 12:43:07 -0500

    stop copy and card deploy for now

--- app_config.py
+++ app_config.py
@@ -290,11 +290,11 @@ def configure_targets(deployment_target):
         NEXT_ELECTION_DATE = '2016-03-01'
         ELEX_FLAGS = ''
         ELEX_DELEGATE_FLAGS = ''
-        COPY_DEPLOY_INTERVAL = 30
+        COPY_DEPLOY_INTERVAL = 0
         RESULTS_DEPLOY_INTERVAL = 60
-        CARD_DEPLOY_INTERVAL = 30
+        CARD_DEPLOY_INTERVAL = 0
         SITE_ARCHIVE_INTERVAL = 0
-        DELEGATES_DEPLOY_INTERVAL = 3600
+        DELEGATES_DEPLOY_INTERVAL = 0
     elif deployment_target == 'test':
         S3_BUCKET = 'stage-elections16.apps.npr.org'
         S3_BASE_URL = 'http://stage-elections16.apps.npr.org.s3-website-us-east-1.amazonaws.com'
