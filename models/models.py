2f81c1f84fc655a4c8dfc681d20afb143942e908
David Eads <davideads@gmail.com>
2016-02-29 15:25:17 -0500

    default true for accept AP

--- models/models.py
+++ models/models.py
@@ -65,7 +65,7 @@ class Result(BaseModel):
 
 class Call(BaseModel):
     call_id = ForeignKeyField(Result, related_name='call')
-    accept_ap = BooleanField(default=False)
+    accept_ap = BooleanField(default=True)
     override_winner = BooleanField(default=False)
 
 
