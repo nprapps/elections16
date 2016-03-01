892a4a39c17e0a6db329a3a2d980392435fee29e
David Eads <davideads@gmail.com>
2016-03-01 18:11:42 -0500

    10s results deploy on prod

--- app_config.py
+++ app_config.py
@@ -274,7 +274,7 @@ def configure_targets(deployment_target):
         ELEX_FLAGS = ''
         ELEX_DELEGATE_FLAGS = ''
         COPY_DEPLOY_INTERVAL = 10
-        RESULTS_DEPLOY_INTERVAL = 0
+        RESULTS_DEPLOY_INTERVAL = 10
         CARD_DEPLOY_INTERVAL = 30
         SITE_ARCHIVE_INTERVAL = 3600
         DELEGATES_DEPLOY_INTERVAL = 600
