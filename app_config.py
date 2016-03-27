70b3a263a1c41ca537471448cdd1b560e2f9371c
David Eads <davideads@gmail.com>
2016-03-27 14:49:22 -0400

    turn off results loader now that AP is done tabulating 3-26 elections

--- app_config.py
+++ app_config.py
@@ -274,7 +274,7 @@ def configure_targets(deployment_target):
         ELEX_FLAGS = ''
         ELEX_DELEGATE_FLAGS = ''
         LOAD_COPY_INTERVAL = 10
-        LOAD_RESULTS_INTERVAL = 10
+        LOAD_RESULTS_INTERVAL = 0
         CARD_DEPLOY_INTERVAL = 10
         SITE_ARCHIVE_INTERVAL = 3600
         LOAD_DELEGATES_INTERVAL = 600
