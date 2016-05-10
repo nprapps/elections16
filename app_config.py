5617a473bb3fc446a4d403fbf6fcc016b24594bd
David Eads <davideads@gmail.com>
2016-05-10 09:57:48 -0400

    switch to NE WV primary

--- app_config.py
+++ app_config.py
@@ -195,7 +195,7 @@ authomatic = Authomatic(authomatic_config, os.environ.get('AUTHOMATIC_SALT'))
 """
 Election configuration
 """
-NEXT_ELECTION_DATE = '2016-05-03'
+NEXT_ELECTION_DATE = '2016-05-10'
 ELEX_FLAGS = ''
 ELEX_DELEGATE_FLAGS = ''
 
