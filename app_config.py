adc771d2a9b018b01333013fb9596f82a3bd0c83
David Eads <davideads@gmail.com>
2016-05-17 11:42:15 -0400

    bump next election date to 5-17

--- app_config.py
+++ app_config.py
@@ -195,7 +195,7 @@ authomatic = Authomatic(authomatic_config, os.environ.get('AUTHOMATIC_SALT'))
 """
 Election configuration
 """
-NEXT_ELECTION_DATE = '2016-05-10'
+NEXT_ELECTION_DATE = '2016-05-17'
 ELEX_FLAGS = ''
 ELEX_DELEGATE_FLAGS = ''
 
